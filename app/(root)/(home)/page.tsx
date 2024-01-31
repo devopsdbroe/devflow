c66b4235d3a9186b63cd35384f4516fb5a77d6ea
devopsdbroe <dbroe1995@gmail.com>
2024-01-31 18:27:28 -0500

    Add component for no results

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,9 +1,39 @@
 import HomeFilters from "@/components/home/HomeFilters";
 import Filter from "@/components/shared/Filter";
+import NoResult from "@/components/shared/NoResult";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
+
+const questions = [
+	{
+		_id: 1,
+		title: "Test question 1",
+		tags: [
+			{ _id: 1, name: "python" },
+			{ _id: 2, name: "sql" },
+		],
+		author: "John Doe",
+		upvotes: 10,
+		views: 100,
+		answers: 2,
+		createdAt: "2021-09-01T12:00:00.000Z",
+	},
+	{
+		_id: 2,
+		title: "Test question 2",
+		tags: [
+			{ _id: 1, name: "react" },
+			{ _id: 2, name: "sql" },
+		],
+		author: "John Doe",
+		upvotes: 10,
+		views: 100,
+		answers: 2,
+		createdAt: "2021-09-01T12:00:00.000Z",
+	},
+];
 
 export default function Home() {
 	return (
@@ -35,6 +65,21 @@ export default function Home() {
 			</div>
 
 			<HomeFilters />
+
+			<div className="mt-10 flex w-full flex-col gap-6">
+				{questions.length > 0 ? (
+					questions.map((question) => "QuestionCard")
+				) : (
+					<NoResult
+						title="There's no questions to show"
+						description="Be the first to break the silence! Ask a question and kickstart the
+						discussion. Your query could be the next thing others learn from. Get
+						involved!"
+						link="/ask-question"
+						linkTitle="Ask a Question"
+					/>
+				)}
+			</div>
 		</>
 	);
 }
