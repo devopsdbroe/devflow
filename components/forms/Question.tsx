6ed7def823a880c8ed7e268c700f729a346e4d80
devopsdbroe <dbroe1995@gmail.com>
2024-03-08 19:17:47 -0500

    Create Answer Form

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -21,6 +21,7 @@ import { Badge } from "@/components/ui/badge";
 import Image from "next/image";
 import { createQuestion } from "@/lib/actions/question.action";
 import { useRouter, usePathname } from "next/navigation";
+import { useTheme } from "@/context/ThemeProvider";
 
 const type: any = "create";
 
@@ -29,6 +30,8 @@ interface QuestionProps {
 }
 
 const Question = ({ mongoUserId }: QuestionProps) => {
+	const { mode } = useTheme();
+
 	const editorRef = useRef(null);
 	const [isSubmitting, setIsSubmitting] = useState(false);
 
@@ -179,6 +182,8 @@ const Question = ({ mongoUserId }: QuestionProps) => {
 											"codesample | bold italic forecolor | alignleft aligncenter |" +
 											"alignright alignjustify | bullist numlist",
 										content_style: "body { font-family:Inter; font-size:16px }",
+										skin: mode === "dark" ? "oxide-dark" : "oxide",
+										content_css: mode === "dark" ? "dark" : "light",
 									}}
 								/>
 							</FormControl>
