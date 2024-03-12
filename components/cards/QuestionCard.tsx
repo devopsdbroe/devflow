fe1fb899d7e56cf237a9dedcf2986de2908d5d3e
devopsdbroe <dbroe1995@gmail.com>
2024-03-12 18:19:50 -0400

    Implement Edit/Delete Question/Answer component

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import RenderTag from "@/components/shared/RenderTag";
 import Metric from "@/components/shared/Metric";
 import { formatAndDivideNumber, getTimestamp } from "@/lib/utils";
+import { SignedIn } from "@clerk/nextjs";
+import EditDeleteAction from "../shared/EditDeleteAction";
 
 interface QuestionCardProps {
 	_id: string;
@@ -33,6 +35,8 @@ const QuestionCard = ({
 	answers,
 	createdAt,
 }: QuestionCardProps) => {
+	const showActionButtons = clerkId && clerkId === author.clerkId;
+
 	return (
 		<div className="card-wrapper rounded-[10px] p-9 sm:px-11">
 			<div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
@@ -46,7 +50,14 @@ const QuestionCard = ({
 						</h3>
 					</Link>
 				</div>
-				{/* TODO: If signed in - add edit/delete actions */}
+				<SignedIn>
+					{showActionButtons && (
+						<EditDeleteAction
+							type="Question"
+							itemId={JSON.stringify(_id)}
+						/>
+					)}
+				</SignedIn>
 			</div>
 			<div className="mt-3.5 flex flex-wrap gap-2">
 				{tags.map((tag) => (
