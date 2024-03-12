1efe3657b52447b956dd000912dd5943fe406ccf
devopsdbroe <dbroe1995@gmail.com>
2024-03-11 21:26:12 -0400

    Implement User Answers Tab

--- components/shared/AnswerTab.tsx
+++ components/shared/AnswerTab.tsx
@@ -1,4 +1,6 @@
+import { getUserAnswers } from "@/lib/actions/user.action";
 import { SearchParamsProps } from "@/types";
+import AnswerCard from "../cards/AnswerCard";
 
 interface QuestionTabProps extends SearchParamsProps {
 	userId: string;
@@ -10,6 +12,22 @@ const AnswerTab = async ({
 	userId,
 	clerkId,
 }: QuestionTabProps) => {
-	return <div>AnswerTab</div>;
+	const result = await getUserAnswers({ userId, page: 1 });
+
+	return (
+		<>
+			{result.answers.map((answer) => (
+				<AnswerCard
+					key={answer._id}
+					clerkId={clerkId}
+					_id={answer._id}
+					question={answer.question}
+					author={answer.author}
+					upvotes={answer.upvotes.length}
+					createdAt={answer.createdAt}
+				/>
+			))}
+		</>
+	);
 };
 export default AnswerTab;
