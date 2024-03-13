66eff3e2e08fd4185e0a0f6f282e11f227900eb9
devopsdbroe <dbroe1995@gmail.com>
2024-03-13 19:28:22 -0400

    Show top questions

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -1,36 +1,21 @@
 import Image from "next/image";
 import Link from "next/link";
 import RenderTag from "./RenderTag";
+import { getHotQuestions } from "@/lib/actions/question.action";
 
-const RightSidebar = () => {
-	const hotQuestions = [
-		{
-			_id: "1",
-			title:
-				"Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?",
-		},
-		{ _id: "2", title: "Is it only me or the font is bolder than necessary?" },
-		{ _id: "3", title: "Can I get the course for free?" },
-		{ _id: "4", title: "Redux Toolkit Not Updating State as Expected" },
-		{ _id: "5", title: "Async/Await Function Not Handling Errors Properly" },
-	];
+const RightSidebar = async () => {
+	const hotQuestions = await getHotQuestions();
 
-	const popularTags = [
-		{ _id: "1", name: "javascript", totalQuestions: 5 },
-		{ _id: "2", name: "react", totalQuestions: 5 },
-		{ _id: "3", name: "next", totalQuestions: 5 },
-		{ _id: "4", name: "vue", totalQuestions: 2 },
-		{ _id: "5", name: "redux", totalQuestions: 10 },
-	];
+	const popularTags = [];
 
 	return (
-		<section className="background-light900_dark200 light-border sticky right-0 top-0 flex h-screen flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden custom-scrollbar w-[350px]">
+		<section className="background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden">
 			<div>
 				<h3 className="h3-bold text-dark200_light900">Top Questions</h3>
 				<div className="mt-7 flex w-full flex-col gap-[30px]">
 					{hotQuestions.map((question) => (
 						<Link
-							href={`/questions/${question._id}`}
+							href={`/question/${question._id}`}
 							key={question._id}
 							className="flex cursor-pointer items-center justify-between gap-7"
 						>
