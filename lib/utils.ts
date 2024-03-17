8a5525c125b797a0867352e47f2165c9944f04d0
devopsdbroe <dbroe1995@gmail.com>
2024-03-17 16:18:41 -0400

    Implement badge system

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,9 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import qs from "query-string";
+import { BADGE_CRITERIA } from "@/constants";
+import { BadgeCounts } from "@/types";
+import { M_PLUS_1 } from "next/font/google";
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
@@ -111,4 +114,34 @@ export const removeKeysFromQuery = ({
 		},
 		{ skipNull: true }
 	);
+};
+
+interface BadgeProps {
+	criteria: {
+		type: keyof typeof BADGE_CRITERIA;
+		count: number;
+	}[];
+}
+
+export const assignBadges = (params: BadgeProps) => {
+	const badgeCounts: BadgeCounts = {
+		GOLD: 0,
+		SILVER: 0,
+		BRONZE: 0,
+	};
+
+	const { criteria } = params;
+
+	criteria.forEach((item) => {
+		const { type, count } = item;
+		const badgeLevels: any = BADGE_CRITERIA[type];
+
+		Object.keys(badgeLevels).forEach((level: any) => {
+			if (count >= badgeLevels[level]) {
+				badgeCounts[level as keyof BadgeCounts] += 1;
+			}
+		});
+	});
+
+	return badgeCounts;
 };
