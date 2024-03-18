1e78759c38bc3fcd62b59fc44f178c6d5cd46dbb
devopsdbroe <dbroe1995@gmail.com>
2024-03-18 16:44:56 -0400

    Setup AI answser featrue

--- components/forms/Answer.tsx
+++ components/forms/Answer.tsx
@@ -30,6 +30,7 @@ interface AnswerProps {
 const Answer = ({ question, questionId, authorId }: AnswerProps) => {
 	const pathname = usePathname();
 	const [isSubmitting, setIsSubmitting] = useState(false);
+	const [isSubmittingAI, setIsSubmittingAI] = useState(false);
 	const { mode } = useTheme();
 	const editorRef = useRef(null);
 
@@ -65,6 +66,30 @@ const Answer = ({ question, questionId, authorId }: AnswerProps) => {
 		}
 	};
 
+	const generateAIAnswer = async () => {
+		if (!authorId) return;
+
+		setIsSubmittingAI(true);
+
+		try {
+			const response = await fetch(
+				`${process.env.NEXT_PUBLIC_SERVER_URL}/api/chatgpt`,
+				{
+					method: "POST",
+					body: JSON.stringify({ question }),
+				}
+			);
+
+			const aiAnswer = await response.json();
+
+			alert(aiAnswer.reply);
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setIsSubmittingAI(false);
+		}
+	};
+
 	return (
 		<div>
 			<div className="flex flex-col justify-between gap-5 sm:flex-row sm:items-center sm:gap-2">
@@ -73,7 +98,7 @@ const Answer = ({ question, questionId, authorId }: AnswerProps) => {
 				</h4>
 				<Button
 					className="btn light-border-2 gap-1.5 rounded-md px-4 py-2.5 text-primary-500 shadow-none dark:text-primary-500"
-					onClick={() => {}}
+					onClick={generateAIAnswer}
 				>
 					<Image
 						src="/assets/icons/stars.svg"
@@ -82,7 +107,7 @@ const Answer = ({ question, questionId, authorId }: AnswerProps) => {
 						height={12}
 						className="object-contain"
 					/>
-					Generate an AI Answer
+					Generate AI Answer
 				</Button>
 			</div>
 			<Form {...form}>
